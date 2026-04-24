eae29437117381cd1b7c78194827dd05d1887411
harshika577 <shankardurge143@gmail.com>
2026-04-24 11:24:16 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,20 @@
+print("🔢 harshika's calculator 🔢")
+print("goal: python narchukoni project cheyyadam")
 
+name = input("Nee peru: ")
+print("hii,", name, ",welcome")
+
+a = int(input("first number: "))
+b = int(input("second number: "))
+
+print("\n___ Result ___")
+print("addition :", a + b)
+print("subtraction:", a - b)
+print("multiplication:", a * b)
+
+if b != 0:
+    print("division:", a / b)
+else:
+    print("0 tho divide cheyyalem")
+
+print("\n goal complete ✅ -Harshika577")
